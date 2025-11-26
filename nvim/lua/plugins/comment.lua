981f77aa0e2851d70b316731da96cd99b4148ab1
hoangtrann <thhoang.tr@gmail.com>
2025-11-26 12:50:22 +0700

    update configs

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,18 +1,11 @@
 return {
   {
     "folke/ts-comments.nvim",
-    enabled = false,
-  },
-  {
-    "nvim-mini/mini.comment",
-    enabled = false,
-    version = "*",
-    config = function()
-      require("mini.comment").setup()
-    end,
+    enabled = true,
   },
   {
     "numToStr/Comment.nvim",
+    enabled = false,
     opts = {},
   },
   {
@@ -20,14 +13,14 @@ return {
     cmd = { "TodoTrouble", "TodoTelescope" },
     event = "LazyFile",
     opts = {},
-  -- stylua: ignore
-  keys = {
-    { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
-    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
-    { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
-    { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-    { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-    { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
-  },
+      -- stylua: ignore
+      keys = {
+        { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+        { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+        { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+        { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+        { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+        { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+      },
   },
 }
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -53,9 +53,21 @@ return {
     end,
   },
   {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        variant = "auto",
+        dark_variant = "main",
+        dim_inactive_windows = false,
+        extend_background_behind_borders = true,
+      })
+    end
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nord",
+      colorscheme = "rose-pine",
     },
   },
 }
